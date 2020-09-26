dfd24763083c262882ed4088418831f6be2e120e
Christan-Dungca <cdungca14@gmail.com>
2020-09-26 21:12:54 +0100

    Changed AUX
    Removed the Aux folder from HOC, and replaced instances with React Fragment.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment} from "react";
 import Styles from './Home.module.scss';
-
-import Aux from '../../hoc/Aux/Aux';
 import Landing from '../../components/Landing/Landing';
 import About from '../../components/About/About';
 import Articles from '../../components/Articles/Articles';
@@ -11,12 +9,12 @@ import Calculator from '../Calculator/Calculator';
 class Home extends Component {
   render() {
     return (
-      <Aux>
+      <Fragment>
         <Landing className={Styles.Landing} />
         <About className={Styles.About} />
         <Articles className={Styles.Landing} /> 
         <Calculator className={Styles.Calculator} />
-      </Aux>
+      </Fragment>
     );
   }
 }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-import Aux from '../../hoc/Aux/Aux';
+
 
 class Layout extends Component {
 
     render () {
         return (
-            <Aux>
+            <Fragment>
                 {/* Navigation will always be shown */}
                 <h1>Navigation Placeholder from Layout</h1>
                 {/* This will change according to App Component */}
@@ -15,7 +15,7 @@ class Layout extends Component {
                 </main>
                 {/* Footer will always be shown */}
                 <h1> Footer Placeholder from Layout </h1>
-            </Aux>
+            </Fragment>
         )
     }
 
