fe94f00bbb35cfe4a859120d40815f9e0539a6c7
Christan-Dungca <cdungca14@gmail.com>
2020-09-30 17:52:18 +0100

    Edited Landing Page and added Global SCSS file
    Created the landing page layout based on the whitespace magazine. Added a SCSS global folder available to be imported in the future.

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import Styles from './Landing.module.scss';
+import React from "react";
+import styles from "./Landing.module.scss";
+import templeImage from "../../assets/images/temple-pink-left.jpg";
+import smallImage from "../../assets/images/green-kimono.jpg"
 
 const Landing = () => {
-    return (
-        <div className={Styles.Landing}>
-            <h3>Landing placeholder from Home</h3>
+  return (
+    <div className={styles.Landing}>
+      {/* Left side of page */}
+      <div className={styles.LandingLeft}>
+        {/* Large image  */}
+        <div className={styles.leftImageContainer}>
+          <img src={templeImage} alt="pink temple" className={styles.image} />
         </div>
-    )
-}
+        {/* Tokyo Traveler Text */}
+        <div className={styles.textContainer}>
+            <h1 className={styles.text}> Tokyo <span> Traveller </span></h1>
+        </div>
+      </div>
+      {/* Right side of page */}
+      <div className={styles.LandingRight}>
+        <div className={styles.contentContainer}>
+          {/* Small Image */}
+          <div className={styles.rightImageContainer}>
+            <img src={smallImage} alt="kimono girl"/>
+          </div>
+          {/* quote container */}
+          <div className={styles.quoteContainer}>
+            <p className={styles.quote}>"One doesn’t discover new lands without consenting to lose sight of the shore for a very long time."</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
+
+
