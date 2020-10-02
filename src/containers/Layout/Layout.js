a3c4e843af30a897979c48094c1afe1ad319d3a6
Christan-Dungca <cdungca14@gmail.com>
2020-10-02 19:33:19 +0100

    Layout Class to Function Component
    Migrated the Layout class component to a react function component which utilizes hooks. The useState hook replaces the state object and handler from before: which keeps track if nav button is clicked or not

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,38 +1,65 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import styles from './Layout.module.scss';
 import Navigation from '../../components/Navigation/Navigation';
 import Menu from '../../components/Menu/Menu';
 
-class Layout extends Component {
+// class Layout extends Component {
 
-    state = {
-        showNav: false
+//     state = {
+//         showNav: false
+//     }
+
+//     // NAVIGATION STATE //
+//     toggleNavHandler = () => { 
+//         console.log(this.state.showNav);
+//         this.setState( ( prevState ) => {
+//             return { showNav: !prevState.showNav };
+//         });
+//     } //pass this method to Navigation component via props
+
+//     render () {
+//         return (
+//             <Fragment>
+//                 {this.state.showNav && <Menu />}
+//                 {/* Navigation will always be shown */}
+//                 <Navigation  className={styles.Navigation} toggleNavHandler={this.toggleNavHandler} showNav={this.state.showNav}/>
+//                 {/* This will change according to App Component */}
+//                 <main>
+//                     {this.props.children}
+//                 </main>
+//                 {/* Footer will always be shown */}
+//                 <h1> Footer Placeholder from Layout </h1>
+//             </Fragment>
+//         )
+//     }
+// }
+
+const Layout = (props) => {
+
+    const [showNav, toggleNav] = React.useState(false);
+
+    const toggleNavHandler = () => {
+        const toggledNav = !showNav;
+        console.log(toggledNav);
+        toggleNav(toggledNav)
     }
 
-    // NAVIGATION STATE //
-    toggleNavHandler = () => { 
-        console.log(this.state.showNav);
-        this.setState( ( prevState ) => {
-            return { showNav: !prevState.showNav };
-        });
-    } //pass this method to Navigation component via props
-
-    render () {
-        return (
-            <Fragment>
-                {this.state.showNav && <Menu />}
+    return (
+        <Fragment>
+                {showNav && <Menu />}
                 {/* Navigation will always be shown */}
-                <Navigation  className={styles.Navigation} toggleNavHandler={this.toggleNavHandler} showNav={this.state.showNav}/>
+                <Navigation  className={styles.Navigation} toggleNavHandler={() => toggleNavHandler()} showNav={showNav}/>
                 {/* This will change according to App Component */}
                 <main>
-                    {this.props.children}
+                    {props.children}
                 </main>
                 {/* Footer will always be shown */}
                 <h1> Footer Placeholder from Layout </h1>
             </Fragment>
-        )
-    }
+    )
 
 }
+
+
 
 export default Layout;
