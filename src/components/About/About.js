4daf8e78601f03f3f155ad7343a0dd7ed99f7b8d
Christan-Dungca <cdungca14@gmail.com>
2020-10-15 12:33:06 +0100

    Added entries for 2nd and 3rd week of august

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,19 +8,22 @@ const About = () => {
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
             <div className={styles.title}> 
-                <h3> “Someone's sitting in the shade today because someone planted a tree a long time ago.”</h3>
+                <h3> “Someone's sitting in the shade <span className={styles.underline}>today</span> because someone planted a tree <span className={styles.underline}>a long time ago</span>.”</h3>
             </div>
             <div className={styles.content}>
                 <div className={styles.contentImage}></div>
                 <div className={styles.contentText}>
                     <p className={styles.text}>Poor planning can lead to an unpleasant travel experience. Tokyo Traveler's aim is to help readers know everything there is to know about travelling to Japan.Don't miss out on the opportunity to make your next trip to Japan, the one to remember. </p>
-                    <a href="#" className={styles.link}>Read more &#8594; </a>
+                    <a href="#" className={`${styles.link}`}>Read more &#8594; </a>
                 </div>
             </div>
         </div>
       </div>
       {/* RIGHT SIDE PICTURE CONTAINER */}
-      <div className={styles.pictureContainer}></div>
+      <div className={styles.pictureContainer}>
+          {/* <div className={styles.overlay}></div> */}
+          <div className={styles.overlay}>tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler tokyo traveler</div>
+      </div>
     </div>
   );
 };
