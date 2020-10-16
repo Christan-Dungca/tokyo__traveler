f68775b4fa4c78ab464275af0640450e3b16f3a3
Christan-Dungca <cdungca14@gmail.com>
2020-10-16 20:34:40 +0100

    Changed Navigation and Landing Component
    Styled the Navigation component to be smaller with smaller text. The menu button is now a hamburger menu. The position is fixed. The Landing component has a single image with text cover it. Added media queries to make the image responsive. Added functionality to change the background image based on the string inside backgroundImages array

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Landing.module.scss";
 
-import smallHatMan from "../../assets/images/small-hat-man.jpg";
-import mediumHatMan from "../../assets/images/medium-hat-man.jpg";
-import largeHatMan from "../../assets/images/large-hat-man.jpg";
+/* 
+    TODO:
+        
+    [x] Create Landing Container > Image Container 
+    [] Change colour of landing to be #212529
+    [x] Add white text Tokyo Traveler across image in uppercase or lowercase?
+    [x] Change the background every second to the three pictures from the array
+    [] Add opaque text in the background
+    [] Add vertical explore with arrow pointing downwards
+    [] On screens smaller 1000, the image will be 60% the width
+    [] On screens smaller than 600, the image will be 100% the width
+*/
 
 const Landing = () => {
+  // State will keep track of which image to use from backgroundImage array
+  const [background, updateBackground] = useState(0);
+
+  // Component below render images from backgroundImage array
+  // const backgroundImages = ['green', 'pink', 'blue'];
+  const backgroundImages = ['smSmallWoman','smSunsetGirl', 'smSumoStadium', 'smTempleSakura'];
+
+  // handleBackground Image is called from useEffect hook to change state
+  const handleBackgroundChange = () => {
+    // If at the end of the backgroundImage array, set count back to 0
+    if (background === backgroundImages.length - 1) {
+      updateBackground(0);
+    } else {
+      updateBackground(background + 1);
+    }
+  };
+
+  //  DON'T UNCOMMENT WHEN TESTING
+  // useEffect will call the handleBackgroundChange function every few seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     handleBackgroundChange();
+  //   }, 1000);
+  //   return () => clearTimeout(timer);
+  // }); // [] will make it run once after mounting
+
   return (
     <div className={styles.Landing}>
-      <div className={styles.imgContainer}></div>
+      {/* Image Centered in the middle of landing Section */}
+      <div className={`${styles.imgContainer} ${styles[backgroundImages[background]]}`}></div>
+      {/* Title centered in middle of landing section*/}
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Tokyo Traveler</h1>
+      </div>
     </div>
   );
 };
 
 export default Landing;
-
+// <div className={styles.Landing}>
+// {/* Background Image */}
+//  <div className={`${styles.imgContainer}`}></div>
+//  {/* Title Container */}
+// <div className={styles.titleContainer}>
+//   <h1 className={styles.title}>Tokyo Traveler</h1>
+//  <p className={styles.subTitle}>A simple travel guide</p>
+//</div>
+// </div>
 
 // const Landing = () => {
-  //   return (
+//   return (
 //     <div className={styles.Landing}>
 //       {/* Left side of page */}
 //       <div className={styles.LandingLeft}>
@@ -47,20 +95,24 @@ export default Landing;
 //   );
 // };
 
-{/* <img
+{
+  /* <img
   src={smallHatMan}
   srcSet={`${smallHatMan} 640w, ${mediumHatMan} 1920w, ${largeHatMan} 2400w`}
   sizes="(min-width: 1000px) 2400px,
          (min-width: 600px) 1920px,
          (max-width: 400px) 640px"
-/> */}
+/> */
+}
 
-{/* <picture>
+{
+  /* <picture>
   <src media="(min-width:1000px)" srcSet={largeHatMan} />
   <src media="(min-width:600px)" srcSet={mediumHatMan} />
   <src media="(min-width:400px)" srcSet={smallHatMan} />
   <img src={largeHatMan}></img>
-</picture> */}
+</picture> */
+}
 
 {
   /* <img src={small} srcSet={`${small} 300w, ${medium} 768w, ${large} 1280w, ${xlarge} 3200w`} onLoad={this.onLoad} /> */
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,19 +6,28 @@ const Navigation = (props) => {
         <nav className={styles.Navigation}>
             <a href="#" 
                 className={styles.logo} 
-                style={ props.showNav === true ? {color: "white"} : {color: "white"}}> Tokyo Traveler </a>
+                style={ props.showNav === true ? {color: "black"} : {color: "black"}}
+            > Tokyo Traveler </a>
             {/* <div className={styles.navBtnContainer} onClick={props.toggleNavHandler}> 
                 <div className={styles.navBtn}></div>  
             </div> */}
-            <div className={styles.navBtnContainer} >
-                {
-                    props.showNav === false 
-                    ? <p className={styles.open} onClick={props.toggleNavHandler}> Menu </p> 
-                    : <p className={styles.close} onClick={props.toggleNavHandler}> Close </p>
-                }
+            <div className={styles.navContainer}>
+                <div className={styles.navBtn}></div>
             </div>
         </nav>
     )
 }
 
 export default Navigation;
+
+/* 
+    TODO:
+        
+    [x] Change sizing of the navigation
+    [x] Change color of navigation background
+    [x] Change positioning of the navigation to be fixed or absolute?
+    [ ] Change positioning of elements within the navigation on smaller screens
+    [x] Switch the menu to be a hamburger menu icon
+    [x] Add a drop shadow to the navigation
+    [] The Navigation is the same colour as the Landing section to begin with, then onScroll it turns white
+*/
