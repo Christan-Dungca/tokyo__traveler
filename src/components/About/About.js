0381cd6ba5d24a4a2e9f28a3d013df26301d5653
Christian Dungca <cdungca14@gmail.com>
2020-10-26 14:39:33 +0000

    Added section to About component
    Added another component to the About Section. Below the double image section, the layout of the new component has a left and right section. The left section contains a header and image while the right section is just text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,17 +3,12 @@ import styles from "./About.module.scss";
 
 /* 
   TODO
-  [x] Create a container for the Text
-  [x] Style container to be a flex container with large text centered in the middle of section
-  [x] Create a container for double image art
-  [x] Have containers each be 50% of the About section
-  [] Creat a grid Layout to overlap the images, no need for the layout to change on smaller screens: it will just get smaller
-  [] Have the big text and overlapping image sections next to each other then try below each other 
-  [] Add additional styling elements to the grid and photos
-  [] ? Add scaling on hover to make some sort of cool interactin
-  [] ? Create a component to separate the About section and Article section
-  [] Picture large left 50% ==> "hello, future travlers" ==> goal of tokyo traveler... etc
-  [] Double Image Large Next to each other
+  [] Add one more section for the about: will give the full rundown on tokyo traveler, maybe a link to a separate page?
+  [x] Create another component for the about section FullDetail Component
+  [x] Container -> Left Container + Right Container
+  [x] Left Container : 80vw 100% (width, height) -> about + image
+  [x] Right Container : 100% 100% -> about text
+  [] Create a Grid layout for About Section
 */
 
 const About = () => {
@@ -36,11 +31,27 @@ const About = () => {
             unpleasant experience. We can help you with that!
           </p>
         </div>
-      </div>
-      {/* Double Image Centered on Page */}
+      </div> 
+      {/* Double Image Component */}
       <div className={styles.imageSection}>
-        <div className={styles.doubleImageContainer}> 
-          <div className={styles.doubleImage}></div>        
+        <div className={styles.doubleImageContainer}>
+          <div className={styles.doubleImage}></div>
+        </div>
+      </div>
+      {/* FullDetail Component */}
+      <div className={styles.FullDetail}>
+        <div className={styles.leftContainer}>
+          <div className={styles.leftAbout}> 
+            <h1 className={styles.leftText}>About</h1>
+          </div>
+          <div className={styles.leftImage}> </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <p className={styles.rightText}>Hello, future travelers! Tokyo Traveler is a guide with the goal of
+            helping you effectivly plan you dream trip to Japan! The early
+            stages of planning a trip can be exhilarating and have you eager to
+            venture out and explore. However, poor planning can lead to an
+            unpleasant experience. We can help you with that!</p>
         </div>
       </div>
     </div>
