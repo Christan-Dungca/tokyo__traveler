1634a144cc141f699ece71ed4cba8dbc984c059f
Christian Dungca <cdungca14@gmail.com>
2020-10-29 10:45:27 +0000

    ACreated a Drag and Scroll functionality
    Created drag and scroll functionality for the slider component using refs and event listeners. Refs and default values are initialized on component mount. Using the useEffect hook to attach event listeners to the slider element.

--- src/containers/Carousel/Carousel.js
+++ src/containers/Carousel/Carousel.js
@@ -1,15 +1,78 @@
 import React from "react";
-import styles from './Carousel.module.scss'
+import { useEffect, useRef} from "react";
+import styles from "./Carousel.module.scss";
 
 const Carousel = () => {
+    const items = useRef(null);
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    
+    useEffect(() => {
+        const slider = items.current;
+        // Listening to mousedown event on the carousel component
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+          });
+        // Listening to mouseleave event on the carousel component
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+          });
+        // Listening to mouseup event on the carousel component
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+          });
+        // Listening to mousemove event on the carousel component
+        slider.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * 1; //scroll-fast
+            slider.scrollLeft = scrollLeft - walk;
+            console.log(walk);
+          });
+    }, [])
+
   return (
-      <div className={styles.items}>
-          <div className={styles.item}></div>
-          <div className={styles.item}></div>
-          <div className={styles.item}></div>
-          <div className={styles.item}></div>
-      </div>
+    <div className={styles.items} ref={items}>
+      <div className={styles.item}>Article</div>
+      <div className={styles.item}>Article</div>
+      <div className={styles.item}>Article</div>
+      <div className={styles.item}>Article</div>
+    </div>
   );
 };
 
 export default Carousel;
+
+/* FROM JS FILE NEED TO CONVERT TO REACT COMPONENT 
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+*/
