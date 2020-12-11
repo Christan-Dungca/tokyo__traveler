118ee0976288fab2840f8bf1663901eb2a010d01
Christian-Dungca <cdungca14@gmail.com>
2020-12-11 19:52:59 +0000

    Separated carousel animation logic into a separate function

--- src/containers/Carousel/Carousel.js
+++ src/containers/Carousel/Carousel.js
@@ -2,53 +2,26 @@ import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Carousel.module.scss";
 import articlesList from "../../data/articles-data.json";
+import createCarousel from "../../animations/carouselAnimation";
 
-const Carousel = ({list}) => {
+const Carousel = () => {
   const items = useRef(null);
-  let isDown = false;
-  let startX;
-  let scrollLeft;
 
   useEffect(() => {
     const slider = items.current;
-    // Listening to mousedown event on the carousel component
-    slider.addEventListener("mousedown", (e) => {
-      isDown = true;
-      slider.classList.add("active");
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    });
-    // Listening to mouseleave event on the carousel component
-    slider.addEventListener("mouseleave", () => {
-      isDown = false;
-      slider.classList.remove("active");
-    });
-    // Listening to mouseup event on the carousel component
-    slider.addEventListener("mouseup", () => {
-      isDown = false;
-      slider.classList.remove("active");
-    });
-    // Listening to mousemove event on the carousel component
-    slider.addEventListener("mousemove", (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = (x - startX) * 1; //scroll-fast
-      slider.scrollLeft = scrollLeft - walk;
-      // console.log(walk);
-    });
+    createCarousel({ slider});
   }, []);
 
   return (
     <div className={styles.items} ref={items}>
       {articlesList.map((article) => {
-        return(
-        <div className={styles.item} key={article.id}>
-          <Link to={`/article/${article.id}`}>
-            <h1>Article</h1>
-          </Link>
-        </div>
-        )
+        return (
+          <div className={styles.item} key={article.id}>
+            <Link to={`/article/${article.id}`}>
+              <h1>Article</h1>
+            </Link>
+          </div>
+        );
       })}
     </div>
   );
