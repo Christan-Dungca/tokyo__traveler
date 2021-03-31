07740800a55e85eaa319cda176350ae7b54ea826
Christian-Dungca <cdungca14@gmail.com>
2021-03-31 10:32:24 +0100

    Feat: Add a form submition handler and installs react-hook-form
    Login component uses react-hook-form to validate form inputs. Onsubmit will call onFormSubmit, an async function, that sends login details to the API.
    
    Login component uses react-hook-form to

--- src/containers/Authentication/Login.js
+++ src/containers/Authentication/Login.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 import styles from "./Login.module.scss";
 
 const Login = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  const onFormSubmit = async (formData) => {
+    const res = await axios.post(
+      "http://localhost:3000/api/users/login",
+      formData
+    );
+    console.log(res);
+  };
+
   return (
     <div className={styles.Login}>
       <div className={styles.loginFormContainer}>
@@ -11,15 +23,32 @@ const Login = () => {
           <h2> Log into your account</h2>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
           <div className={styles.inputContainer}>
-            <label for="email"> E-Mail or Username </label>
-            <input id="email" placeholder="Enter your email or username" />
+            <label htmlFor="email"> E-Mail or Username </label>
+            {errors.email && (
+              <p className={styles.inputError}> A valid email is required </p>
+            )}
+            <input
+              id="email"
+              name="email"
+              type="email"
+              ref={register({ required: true })}
+              placeholder="Enter your email or username"
+            />
           </div>
 
           <div className={styles.inputContainer}>
-            <label for="password"> Password </label>
-            <input id="password" placeholder="Enter your password" />
+            <label htmlFor="password"> Password </label>
+            {errors.password && (
+              <p className={styles.inputError}>A valid password is required</p>
+            )}
+            <input
+              id="password"
+              name="password"
+              ref={register({ required: true })}
+              placeholder="Enter your password"
+            />
           </div>
 
           <button> Login </button>
@@ -27,7 +56,7 @@ const Login = () => {
 
         <div className={styles.registerContainer}>
           <p>Not Yet Registered? </p>
-          <a href="/">Register 	&#8594; </a>
+          <a href="/">Register &#8594; </a>
         </div>
       </div>
     </div>
