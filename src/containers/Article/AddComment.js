dd6dc69a516d44509ec9285489024090048003e8
Christian-Dungca <cdungca14@gmail.com>
2021-05-14 12:50:28 +0100

    Feat: Connect AddComment form to the backend
    AddComment will send a post request to the backend and if valid, a comment will be created holding reference to the current logged in user and current article.

--- src/containers/Article/AddComment.js
+++ src/containers/Article/AddComment.js
@@ -2,30 +2,51 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+import useHttpClient from "../../hooks/useHttp";
 import AuthContext from "../../context/auth-context";
 import styles from "./AddComment.module.scss";
 
 const AddComment = () => {
   // use id for the request
-  const { handleSubmit, register, error } = useForm({
-    mode: "onSubmit",
-    reValidateMode: "onChange",
-    defaultValues: "",
-  });
-  const { user } = useContext(AuthContext);
+  const { handleSubmit, register, errors, setValue } = useForm();
   let { id } = useParams();
+  const { user, token } = useContext(AuthContext);
+  const { sendRequest, isLoading } = useHttpClient();
 
-  console.log(user);
+  const handleFormSubmit = async (data) => {
+    if (data.comment.length < 1) return;
+
+    const { comment } = data;
+
+    const response = await sendRequest(
+      `http://localhost:5000/api/articles/${id}/comments`,
+      "POST",
+      { comment, user: user._id },
+      { Authorization: `Bearer ${token}` }
+    );
+    setValue("comment", "");
+    console.log(response);
+  };
+  
   return (
     <div className={styles.Comment}>
-      <form>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
         <div className={styles.container}>
           <div className={styles.imgContainer}></div>
           <div className={styles.commentContainer}>
             <label htmlFor="comment">Post A Comment</label>
+            {errors !== undefined && errors.comment && (
+              <p className={styles.inputError}>
+                A comment must be between 1-250 characters long
+              </p>
+            )}
             <input
-              {...register("comment", { required: true })}
-              placeholder="Add your comment here..."></input>
+              {...register("comment", {
+                required: true,
+                minLength: 1,
+                maxLength: 255,
+              })}
+            />
           </div>
         </div>
         <button>Post Comment</button>
